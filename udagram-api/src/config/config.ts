48f2590fc97c6e0268fb98fe5faba2ddddf80560
Ahmed-bastwesy <ahmed.reda.bastawesy@gmail.com>
2022-05-18 09:57:58 +0200

    remove static environment

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -7,18 +7,18 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: process.env.POSTGRES_USERNAME || `postgres`,
-  password: process.env.POSTGRES_PASSWORD  || 'postgres',
-  database: process.env.POSTGRES_DB || "postgres",
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   port: Number(process.env.PORT) ,
-  dbPort: Number(process.env.DB_PORT) || 5432 ,
-  host: process.env.POSTGRES_HOST || "database-1.cpwyivf44ots.us-east-1.rds.amazonaws.com",
+  dbPort: Number(process.env.DB_PORT),
+  host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL ||"http://localhost",
+  url: process.env.URL,
   jwt: {
-    secret: process.env.JWT_SECRET || "no_secret",
+    secret: process.env.JWT_SECRET ,
   },
 };
